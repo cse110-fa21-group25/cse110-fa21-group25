838b8a861a35f39e53548bd2314cd2d3e531921f
Quoc Do <qdo@ucsd.edu>
2021-11-17 11:10:36 -0800

    create keyvalue pairs for all form elements

--- source/assets/scripts/recipe-form.js
+++ source/assets/scripts/recipe-form.js
@@ -15,8 +15,39 @@ function init() {
     const remove_step_button = document.querySelector("#remove-step");
     remove_step_button.addEventListener("click", remove_step);
 
+    //Add event listener for Submitting the Form
+    const form_submitted = document.querySelector("#recipeForm");
+    form_submitted.addEventListener("submit", buildJSON);
 }
+
 window.addEventListener("DOMContentLoaded", init);
+
+/**
+ *  Function to build the JSON from the HTML form data.
+ *  @param {event} event - The event that the event listener returns.
+ */
+function buildJSON(event) {
+    //prevent submit button from refreshing the page.
+    event.preventDefault();
+    console.log("building JSON");
+    
+    //Query select all the "input" elements within #recipeForm, make an array out of it  
+    // in order to run reduce on it
+    const recipeInput = Array.from(document.querySelectorAll("#recipeForm input"));
+   
+    //Take all the inputs and make it a key value pair
+    const formattedInputs  = recipeInput.reduce((acc,input) => ({...acc,[input.id]: input.value}), {});
+    console.log(formattedInputs);
+
+    //Take all the "textarea" elements within #recipeFor, repeat the above steps
+    const recipeTextArea = Array.from(document.querySelectorAll("#recipeForm textarea"));
+    const formattedTextArea = recipeTextArea.reduce((acc,textarea) => 
+        ({...acc,[textarea.id]: textarea.value}), {});
+    console.log(formattedTextArea);
+    
+}    
+
+
 
 
 /**
