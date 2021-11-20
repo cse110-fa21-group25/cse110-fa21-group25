c538bf2ece173e60b790c686a1f06b170726785c
Ruby Celeste Marroquin <rcmarroq21@gmail.com>
2021-11-19 20:31:59 -0800

    Fixed linting issues

--- source/assets/scripts/crud.js
+++ source/assets/scripts/crud.js
@@ -7,102 +7,107 @@ const db = firebase.firestore();
 
 /**
  * function retrieves all of the existing recipes
- * @returns a promise for all of the recipe
+ * @return {Promise} a promise for all of the recipe
  */
-function getAllRecipes() {
-  return db.collection("recipes").get().then((querySnapshot) => {
-      const results = []
-      querySnapshot.forEach((doc) => {
-          results.push({id: doc.id, data: doc.data()})
+function getAllRecipes() { // eslint-disable-line no-unused-vars
+  return db.collection('recipes').get().then((querySnapshot) => {
+    const results = [];
+    querySnapshot.forEach((doc) => {
+      results.push({id: doc.id, data: doc.data()});
+    });
+    return results;
+  });
+}
+
+/**
+ * function takes in a recipe ID and searches the database for an
+ * existing recipe
+ * @param {string} recipeId the document id
+ * @return {Promise} a promise for the specific recipe
+ */
+function getRecipeById(recipeId) { // eslint-disable-line no-unused-vars
+  return db.collection('recipes').doc(recipeId).get().then((querySnapshot) => {
+    return {id: querySnapshot.id, data: querySnapshot.data()};
+  });
+}
+
+/**
+ * function that gets recipes based on name of recipe
+ * @param {string} title name of recipe
+ * @return {Promise} a promise for all the recipes containing the given title
+ */
+function getRecipesByTitle(title) { // eslint-disable-line no-unused-vars
+  return db.collection('recipes').get().then((querySnapshot) => {
+    const results = [];
+    querySnapshot.forEach((doc) => {
+      const resultTitle = doc.data().title;
+      if (resultTitle.toLowerCase().includes(title.toLowerCase())) {
+        results.push({id: doc.id, data: doc.data()});
+      }
+    });
+    return results;
+  });
+}
+
+/**
+ * function that gets recipes based on tags
+ * @param {string} tag tags associated with recipe
+ * @return {Promise} a promise for all the recipes containing tags
+ */
+function getRecipesByTag(tag) { // eslint-disable-line no-unused-vars
+  return db.collection('recipes')
+      .where('tags', 'array-contains', tag)
+      .get().then((querySnapshot) => {
+        const results = [];
+        querySnapshot.forEach((doc) => {
+          results.push({id: doc.id, data: doc.data()});
+        });
+        return results;
       });
-      return results;
-  });
 }
 
 /**
- * function takes in a recipe ID and searches the database for an existing recipe
- * @param recipeID the document id
- * @returns a promise for the specific recipe
+ * function creates a new recipe in the firestore database
+ * @param {object} recipeData the recipe document
+ * @return {Promise} a promise for the creation of the given recipe
  */
-function getRecipeById(recipeId) {
-  return db.collection("recipes").doc(recipeId).get().then((querySnapshot) => {
-      return{id: querySnapshot.id, data: querySnapshot.data()} ;
-  });
-}
-
-/**
- * Function
- * @param title name of recipe
- * @returns a promise for all the recipes containing the given title
- */
-function getRecipesByTitle(title) {
-  return db.collection("recipes").get().then((querySnapshot) => {
-      const results = []
-      querySnapshot.forEach((doc) => {
-        const resultTitle = doc.data().title;
-        if (resultTitle.toLowerCase().includes(title.toLowerCase()))
-          results.push({id: doc.id, data: doc.data()})
+function createRecipe(recipeData) { // eslint-disable-line no-unused-vars
+  return db.collection('recipes').doc().set(recipeData)
+      .then(() => {
+        return console.log('Document successfully written!');
+      })
+      .catch((error) => {
+        console.error('Error creating document: ', error);
       });
-      return results
-  });
 }
 
 /**
- * Function
- * @param tag tags associated with recipe
- * @returns a promise for all the recipes containing tags
+ * function takes in a recipe object and updates based on modifications
+ * by user
+ * @param {object} recipe object include both id & data (document id & document)
+ * @return {Promise} a promise for the modification of the given recipe
  */
-function getRecipesByTag(tag) {
-  return db.collection("recipes").where("tags", "array-contains", tag).get().then((querySnapshot) => {
-      const results = []
-      querySnapshot.forEach((doc) => {
-          results.push({id: doc.id, data: doc.data()})
+function updateRecipe(recipe) { // eslint-disable-line no-unused-vars
+  return db.collection('recipes').doc(recipe.id).set(recipe.data).then(() => {
+    return console.log('Document successfully written!');
+  })
+      .catch((error) => {
+        console.error('Error updating document: ', error);
       });
-      return results
-  });
 }
 
 /**
- * Function
- * @param recipeData the recipe document
- * @returns a promise for the creation of the given recipe 
+ * function takes in a recipe ID and searches the database for an
+ * existing recipe and deletes it from the database
+ * @param {string} recipeId the document id
+ * @return {Promise} a promise for the deletion of the given recipe
  */
-function createRecipe(recipeData) {
-  return db.collection("recipes").doc().set(recipeData)
-  .then(() => {
-      return console.log("Document successfully written!");
+function deleteRecipeById(recipeId) { // eslint-disable-line no-unused-vars
+  return db.collection('recipes').doc(recipeId).delete().then(() => {
+    return console.log('Document successfully written!');
   })
-  .catch((error) => {
-      console.error("Error creating document: ", error);
-  });
-}
-
-/**
- * function takes in a recipe object and 
- * @param recipe object include both id & data (document id & document)
- * @returns a promise for the modification of the given recipe 
- */
-function updateRecipe(recipe) {
-   return db.collection("recipes").doc(recipe.id).set(recipe.data).then(() => {
-      return console.log("Document successfully written!");
-  })
-  .catch((error) => {
-      console.error("Error updating document: ", error);
-  });
-}
-
-/**
- * function takes in a recipe ID and searches the database for an existing recipe
- * and deletes it from the database
- * @param recipeId the document id
- * @returns a promise for the deletion of the given recipe 
- */
-function deleteRecipeById(recipeId) {
-  return db.collection("recipes").doc(recipeId).delete().then(() => {
-      return console.log("Document successfully written!");
-  })
-  .catch((error) => {
-      console.error("Error deleting recipe: ", error);
-  });
+      .catch((error) => {
+        console.error('Error deleting recipe: ', error);
+      });
 }
 
--- source/assets/scripts/userAuthentication.js
+++ source/assets/scripts/userAuthentication.js
@@ -1,20 +1,23 @@
-function logout() {
+/**
+ * function that allows the user to logout if signed in
+ */
+function logout() { // eslint-disable-line no-unused-vars
   firebase.auth().signOut().then(() => {
-    console.log('successfully signed out')
-  }).catch((error) => {})
+    console.log('successfully signed out');
+  }).catch((error) => {});
 }
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    const loginLink = document.getElementById('login')
-    const logoutLink = document.getElementById('logout')
+    const loginLink = document.getElementById('login');
+    const logoutLink = document.getElementById('logout');
     loginLink.setAttribute('hidden', true);
     logoutLink.removeAttribute('hidden');
   } else {
     // No user is signed in.
-    const loginLink = document.getElementById('login')
-    const logoutLink = document.getElementById('logout')
+    const loginLink = document.getElementById('login');
+    const logoutLink = document.getElementById('logout');
     loginLink.removeAttribute('hidden');
     logoutLink.setAttribute('hidden', true);
   }
