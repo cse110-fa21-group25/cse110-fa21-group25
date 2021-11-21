7eb7ab38821cdc36883920f4ca876647553a3adf
Justin Nguyen <justnguyen1@gmail.com>
2021-11-21 15:35:24 -0800

    getting data from database

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -1,12 +1,5 @@
 /* eslint-disable guard-for-in */
-// get recipes
-const recipesJSON = [
-  'https://introweb.tech/assets/json/ghostCookies.json',
-  'https://introweb.tech/assets/json/birthdayCake.json',
-  'https://introweb.tech/assets/json/chocolateChip.json',
-];
-
-const recipeData = {};
+let recipeData;
 
 window.addEventListener('DOMContentLoaded', init);
 /**
@@ -14,7 +7,7 @@ window.addEventListener('DOMContentLoaded', init);
  */
 async function init() {
   try {
-    await fetchRecipes();
+    recipeData = await getAllRecipes();
   } catch (e) {
     throw (e);
   }
@@ -23,36 +16,13 @@ async function init() {
   createRecipeCard();
 }
 
-/**
- * Getting all the recipe data from the recipesJson array.
- * @return {Promise}
- */
-async function fetchRecipes() {
-  return new Promise((resolve, reject)=>{
-    recipesJSON.forEach((recipe) => {
-      fetch(recipe)
-          .then((response) => response.json())
-          .then((data) => {
-            recipeData[recipe] = data;
-            if (Object.keys(recipeData).length == recipesJSON.length) {
-              resolve();
-            }
-          })
-          .catch((e)=>{
-            console.error(e);
-            reject(e);
-          });
-    });
-  });
-}
-
 // TODO: SPLIT BASED ON HOME-PAGE CATEGORIES
 // (CLARIFY CATEGORIES (e.g. trending recipes, etc.))
 /**
  * Creating recipe cards from the recipeData.
  */
 async function createRecipeCard() {
-  for (const recipe in recipeData) {
+  for (const recipe of recipeData) {
     // Card DOM Structure
     /* *********************************** *
          * card format:
@@ -78,42 +48,36 @@ async function createRecipeCard() {
     cardDiv.classList.add('col-md-3');
     cardDiv.classList.add('col-sm-6');
     // cardDiv.setAttribute('style', "width: 18rem;")
-
     const thumbnailImg = document.createElement('img');
-    thumbnailImg.setAttribute('src', getImage(recipeData[recipe]));
+    thumbnailImg.setAttribute('src', recipe.data.imageURL);
 
     const cardBodyDiv = document.createElement('div');
     cardBodyDiv.classList.add('card-body');
 
     const recipeTitleH4 = document.createElement('h4');
-    recipeTitleH4.innerHTML = getTitle(recipeData[recipe]);
+    recipeTitleH4.innerHTML = recipe.data.name;
 
     const timeP = document.createElement('p');
-    timeP.innerHTML = formatTime( getTime(recipeData[recipe]) );
+    timeP.innerHTML = formatTime(recipe.data.cookTime);
 
     const recipeOwnerP = document.createElement('p');
-    // TODO: helper function for getRecipeOwner
-    const getRecipeOwner = 'Gordon Ramsay 2.0';
-    recipeOwnerP.innerHTML = 'By ' + getRecipeOwner.italics();
+    recipeOwnerP.innerHTML = 'By ' + recipe.data.author.italics();
 
     // for actual recipes from our app, use loop to check for all tags
     const tagDiv = document.createElement('div');
     tagDiv.classList.add('tags');
-    const tag1Button = document.createElement('button');
-    const tag2Button = document.createElement('button');
-    // TODO: helper function for getTag
-    tag1Button.innerHTML = '#tag1';
-    tag2Button.innerHTML = '#tag2';
-
+    for (const tag in recipe.data.tags) {
+      const tagButton = document.createElement('button');
+      tagButton.innerHTML = recipe.data.tags[tag];
+      tagDiv.appendChild(tagButton);
+    }
 
     const cardFooterDiv = document.createElement('div');
     cardFooterDiv.classList.add('card-footer');
 
-
     // SPA Button -- TODO: display recipe details
     const recipeDetailButton = document.createElement('button');
     recipeDetailButton.innerHTML = 'View Recipe';
-
 
     // Assemble Recipe Card's DOM (as above structure reference)
     cardDiv.appendChild(cardBodyDiv);
@@ -127,9 +91,6 @@ async function createRecipeCard() {
 
     cardFooterDiv.appendChild(recipeDetailButton);
 
-    tagDiv.appendChild(tag1Button);
-    tagDiv.appendChild(tag2Button);
-
     // Attach to the appropriate recipe-row category
     const exampleRecipeRow = document.querySelector(
         '#example-recipe > .recipe-row');
@@ -137,69 +98,6 @@ async function createRecipeCard() {
     exampleRecipeRow.appendChild(cardDiv);
   }
   console.log('done looping through recipes');
-}
-
-/**
- * Get image from recipe's JSON object
- * @param {Object} data: JSON Object
- * @return {String} recipe's image URL if exists, else null
- **/
-function getImage(data) {
-  if (data.image?.url) return data.image.url;
-  if (data.image?.contentUrl) return data.image.contentUrl;
-  if (data.image?.thumbnail) return data.image.thumbnail;
-  if (data['@graph']) {
-    for (let i = 0; i < data['@graph'].length; i++) {
-      if (data['@graph'][i]['@type'] == 'ImageObject') {
-        if (data['@graph'][i]['url']) return data['@graph'][i]['url'];
-        if (data['@graph'][i]['contentUrl']) {
-          return data['@graph'][i]['contentUrl'];
-        }
-        if (data['@graph'][i]['thumbnailUrl']) {
-          return data['@graph'][i]['thumbnailUrl'];
-        }
-      };
-    }
-  }
-  return null;
-}
-
-/**
- * Get title from recipe's JSON object
- * @param {Object} data: JSON Object
- * @return {String} recipe's title if exists, else null
- **/
-function getTitle(data) {
-  if (data.name) return data.name;
-  if (data['@graph']) {
-    for (let i = 0; i < data['@graph'].length; i++) {
-      if (data['@graph'][i]['@type'] == 'Recipe') {
-        if (data['@graph'][i]['name']) return data['@graph'][i]['name'];
-      };
-    }
-  }
-  return null;
-}
-
-/**
- * Get total time from recipe's JSON object
- * @param {Object} data: JSON Object
- * @return {String} recipe's total time to cook/prep
- **/
-function getTime(data) {
-  let time;
-  Object.keys(data).some((key)=>{
-    if (key == 'totalTime') {
-      time = data[key];
-      return true;
-    }
-    // recurse on data to get time
-    if (data[key] && typeof data[key] == 'object') {
-      time = getTime(data[key]);
-      return time !== undefined;
-    }
-  });
-  return time;
 }
 
 /**
@@ -225,5 +123,3 @@ function formatTime(time) {
   return timeFormat.join('');
 }
 
-
-// TODO: GET RECIPE'S OWNER FUNCTION
